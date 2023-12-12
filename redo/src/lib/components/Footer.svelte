027f466cae771a58feb79543aa09a3415b591849
zatzby <128881293+zatzby@users.noreply.github.com>
2023-12-11 22:33:29 -0800

    added svelte 'redo' folder

---
+++ redo/src/lib/components/Footer.svelte
@@ -0,0 +1,35 @@
+<script>
+    // JavaScript logic for the Footer component (if needed)
+    
+  </script>
+  
+  <style>
+    .footer {
+      background-color: black;
+      text-align: center;
+      padding: 5px;
+      position: fixed;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 80px;
+      font-family: Arial, sans-serif;
+    }
+  
+    .footer a {
+      color: #333;
+      text-decoration: none;
+    }
+  
+    .github-badge {
+      margin-top: 10px;
+      width: 50px;
+      height: 50px;
+    }
+  </style>
+  
+  <footer class="footer">
+    <a href="https://github.com/zatzby/nostr.rehab" target="_blank">
+        <img class="github-badge" src="https://i.nostr.build/aD7L.png" alt="Github Logo">
+    </a>
+  </footer>
---
+++ redo/src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import Footer from '$lib/components/Footer.svelte'; 
+  </script>
+  
+  <style>
+    .container {
+      background-image: url('https://nostr.build/p/nb4903.jpeg');
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover;
+      font-family: cypherpunk, sans-serif;
+      font-size: 75px;
+      color: orange;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: calc(100vh - 80px);
+      width: 100%;
+    }
+  
+    a {
+      color: blue;
+      text-decoration: underline;
+    }
+  
+    @media screen and (max-width: 1024px) {
+      .container { font-size: 40px; }
+    }
+    @media screen and (max-width: 768px) {
+      .container { font-size: 30px; }
+    }
+    @media screen and (max-width: 480px) {
+      .container { font-size: 20px; }
+    }
+  </style>
+  
+  <div class="container">
+    <a href="https://nostr.build/p/nb3918.mp4">Get</a> nostr.rehab
+  </div>
+  
+  <Footer />
